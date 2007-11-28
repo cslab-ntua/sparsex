efa57feacaf6412bb9ca133ae57888e471166c07
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2007-11-28 19:30:24 +0200

    use aligned pointers for indices

--- dynarray.h
+++ dynarray.h
@@ -8,6 +8,7 @@ void *dynarray_create(unsigned long elem_size, unsigned long alloc_grain);
 void *dynarray_alloc(struct dynarray *da);
 void dynarray_dealloc(struct dynarray *da);
 void *dynarray_alloc_nr(struct dynarray *da, unsigned long nr);
+void *dynarray_alloc_nr_aligned(struct dynarray *da, unsigned long nr, unsigned long align);
 void dynarray_dealloc_nr(struct dynarray *da, unsigned long nr);
 void *dynarray_get(struct dynarray *da, unsigned long idx);
 void *dynarray_get_last(struct dynarray *da);
