bc732197169a9601bc2e1a2d3f7192b3339085e4
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2008-11-14 18:34:15 +0200

    pyspm: compile with out-of-the-source libs

--- python/setup.py
+++ python/setup.py
@@ -1,12 +1,19 @@
 from distutils.core import setup, Extension
 
+import os
+
+dynarray_dir = os.popen('rsrc resource dynarray').read()[:-1]
+dynarray_dep = dynarray_dir + '/dynarray.o'
+
 setup(
 	name="pyspm", 
 	version="0.001", 
 	ext_modules=[ Extension(
 		"pyspm", 
 		["pyspm.c"],
-		include_dirs=["../"],
-		extra_objects = ["../libspmv.o", "../dynarray.o"]
+		include_dirs=["../", dynarray_dir],
+		extra_objects = ["../libspmv.o", dynarray_dep],
+		extra_link_args = ['-Xlinker', '--allow-multiple-definition'],
+
 	)]
 )
