117361635ff7e9682d554f1646f4f80dc9daa0f2
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-01-19 23:59:03 +0200

    vector: macros for vector.h

--- vector.h
+++ vector.h
@@ -8,29 +8,28 @@
 #define ELEM_TYPE double
 #endif
 
+#define _VNAME(elem_type, name) vector_ ## elem_type ## _ ## name
+#define _VTYPE(elem_type) _VNAME(elem_type, t)
+
+#define DECLARE_VECTOR(vtype) \
+typedef struct { \
+	vtype *elements; \
+	unsigned long size; \
+} _VTYPE(vtype); \
+\
+_VTYPE(vtype) *_VNAME(vtype,create)(unsigned long size); \
+void _VNAME(vtype,destroy)(_VTYPE(vtype) *v); \
+void _VNAME(vtype,init)(_VTYPE(vtype) *v, vtype val); \
+void _VNAME(vtype,init_rand_range)(_VTYPE(vtype) *v, vtype max, vtype min); \
+int _VNAME(vtype,compare)(_VTYPE(vtype) *v1, _VTYPE(vtype) *v2);
+
+DECLARE_VECTOR(float)
+DECLARE_VECTOR(double)
+
+#undef _VNAME
+#undef _VTYPE
+
 #define VECTOR_NAME(name) CON3(vector_, ELEM_TYPE, name)
 #define VECTOR_TYPE VECTOR_NAME(_t)
-
-typedef struct {
-	float *elements;
-	unsigned long size;
-} vector_float_t;
-
-vector_float_t *vector_float_create(unsigned long size);
-void vector_float_destroy(vector_float_t *v);
-void vector_float_init(vector_float_t *v, float val);
-void vector_float_init_rand_range(vector_float_t *v, float max, float min);
-int vector_float_compare(vector_float_t *v1, vector_float_t *v2);
-
-typedef struct {
-	double *elements;
-	unsigned long size;
-} vector_double_t;
-
-vector_double_t *vector_double_create(unsigned long size);
-void vector_double_destroy(vector_double_t *v);
-void vector_double_init(vector_double_t *v, double val);
-void vector_double_init_rand_range(vector_double_t *v, double max, double min);
-int vector_double_compare(vector_double_t *v1, vector_double_t *v2);
 
 #endif /* __VECTOR_H__ */
