4688ed1c7a391f9f8bc2f6ed8ab5deacf1dcde4c
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-02-04 11:08:27 +0200

    csrdu: testing
     - remove matrices with row holes from csrdu_test_all.sh
     - run tests:
       44: segfaulted but can't reproduce
       50: diff: memory exhausted in solar

--- scripts/csrdu_test_all.sh
+++ scripts/csrdu_test_all.sh
@@ -1,5 +1,13 @@
-#!/bin/bash
-for mtx in tests.mtx/* /s/matrices/sorted/???.*.mtx.sorted
+#!/bin/zsh
+
+setopt extended_glob
+
+if [ -z "$1" ]; then
+	mtxfiles=$(echo tests.mtx/* /s/matrices/sorted/(???~(024|041|042)).*.mtx.sorted)
+else
+	mtxfiles="$*"
+fi
+for mtx in $(echo $mtxfiles)
 do
 	echo CSRDU_VERBOSE=0 CSRDU_DE_MINLEN=4 CSRDU_JMP=1 scripts/csrdu_test.sh $mtx
 	     CSRDU_VERBOSE=0 CSRDU_DE_MINLEN=4 CSRDU_JMP=1 scripts/csrdu_test.sh $mtx
@@ -25,4 +33,5 @@ do
 		exit
 	fi
 
+	echo
 done
