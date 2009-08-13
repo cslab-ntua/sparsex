ef32c051b0470f0b0a60e0061842a05b3a0ea4b7
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-08-13 11:24:50 +0300

    patterns: use mt_lib for thread scheduling

--- patterns/spmv.cc
+++ patterns/spmv.cc
@@ -7,6 +7,7 @@
 #include "jit.h"
 
 extern "C" {
+	#include "../mt_lib.h"
 	#include "../spmv_method.h"
 	#include "../spm_crs.h"
 	#include "../spm_mt.h"
@@ -24,27 +25,26 @@ int main(int argc, char **argv)
 	csx_double_t *csx;
 	spm_mt_t spm_mt;
 	spm_mt_thread_t *spm_mt_thread;
-	unsigned int nr_threads;
+	unsigned int threads_nr, *threads_cpus;
 
 	if (argc < 2){
-		std::cerr << "Usage: " << argv[0] << " <mmf_file> (<nr_threads>)\n";
+		std::cerr << "Usage: " << argv[0] << " <mmf_file>\n";
 		exit(1);
 	}
 
-	// use one thread by default
-	nr_threads = (argc > 2) ? atol(argv[2]) : 1;
-	std::cout << "Using " << nr_threads << " threads\n";
+	mt_get_options(&threads_nr, &threads_cpus);
+	std::cout << "Using " << threads_nr << " threads\n";
 
-	Spms = SPM::loadMMF_mt(argv[1], nr_threads);
+	Spms = SPM::loadMMF_mt(argv[1], threads_nr);
 
-	spm_mt.nr_threads = nr_threads;
-	spm_mt.spm_threads = (spm_mt_thread_t *)malloc(sizeof(spm_mt_thread_t)*nr_threads);
+	spm_mt.nr_threads = threads_nr;
+	spm_mt.spm_threads = (spm_mt_thread_t *)malloc(sizeof(spm_mt_thread_t)*threads_nr);
 	if (!spm_mt.spm_threads){
 		perror("malloc");
 		exit(1);
 	}
 
-	for (unsigned int i=0; i < nr_threads; i++){
+	for (unsigned int i=0; i < threads_nr; i++){
 		std::cout << "************ Thread: " << i << "\n";
 
 		Spm = Spms + i;
@@ -54,10 +54,12 @@ int main(int argc, char **argv)
 		DrleMg->EncodeAll();
 		CsxMg = new CsxManager(Spm);
 		Jit = new CsxJit(CsxMg, i);
+
 		spm_mt_thread->spm = csx = CsxMg->mkCsx();
 		Jit->doHooks();
 		spm_mt_thread->spmv_fn = Jit->doJit();
-		spm_mt_thread->cpu = i;
+		spm_mt_thread->cpu = threads_cpus[i];
+
 		delete Jit;
 		delete CsxMg;
 		delete DrleMg;
@@ -72,6 +74,7 @@ int main(int argc, char **argv)
 	spm_crs32_double_destroy(crs);
 	std::cout << "Check Passed\n";
 
+	free(threads_cpus);
 	free(spm_mt.spm_threads);
 	delete[] Spms;
 
