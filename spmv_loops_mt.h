dca01c2d1d640c04700213d286d5593e163632b0
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-08-17 13:49:08 +0300

    stupid ws chagnes

--- spmv_loops_mt.h
+++ spmv_loops_mt.h
@@ -15,11 +15,11 @@ float spmv_float_bench_mt_loop(spm_mt_t *spm_mt, unsigned long loops,
 void spmv_double_check_mt_loop(void *spm, spm_mt_t *spm_mt,
                                spmv_double_fn_t *fn, unsigned long loops,
                                unsigned long rows_nr, unsigned long cols_nr,
-			       spmv_double_fn_t *mt_fn);
+                               spmv_double_fn_t *mt_fn);
 
 void spmv_float_check_mt_loop(void *spm, spm_mt_t *spm_mt,
                               spmv_float_fn_t *fn, unsigned long loops,
                               unsigned long rows_nr, unsigned long cols_nr,
-			      spmv_float_fn_t *mt_fn);
+                              spmv_float_fn_t *mt_fn);
 #endif
 
