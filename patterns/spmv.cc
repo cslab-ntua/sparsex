988a3632f41c272dfc9052beb6884937df0da559
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-08-17 18:22:58 +0300

    patterns/spmv refactor and add benchmark loop

--- patterns/spmv.cc
+++ patterns/spmv.cc
@@ -2,6 +2,7 @@
 #include <cstdlib>
 
 #include "spm.h"
+#include "mmf.h"
 #include "csx.h"
 #include "drle.h"
 #include "jit.h"
@@ -16,46 +17,48 @@ extern "C" {
 
 using namespace csx;
 
-int main(int argc, char **argv)
+static spm_mt_t *getSpmMt(char *mmf_fname)
 {
+	unsigned int threads_nr, *threads_cpus;
+	spm_mt_t *spm_mt;
 	SPM *Spms, *Spm;
 	DRLE_Manager *DrleMg;
 	CsxManager *CsxMg;
 	CsxJit *Jit = NULL;
-	csx_double_t *csx;
-	spm_mt_t spm_mt;
-	spm_mt_thread_t *spm_mt_thread;
-	unsigned int threads_nr, *threads_cpus;
-
-	if (argc < 2){
-		std::cerr << "Usage: " << argv[0] << " <mmf_file>\n";
-		exit(1);
-	}
 
 	mt_get_options(&threads_nr, &threads_cpus);
 	std::cout << "Using " << threads_nr << " threads\n";
 
-	Spms = SPM::loadMMF_mt(argv[1], threads_nr);
-
-	spm_mt.nr_threads = threads_nr;
-	spm_mt.spm_threads = (spm_mt_thread_t *)malloc(sizeof(spm_mt_thread_t)*threads_nr);
-	if (!spm_mt.spm_threads){
+	spm_mt = (spm_mt_t *)malloc(sizeof(spm_mt_t));
+	if (!spm_mt){
 		perror("malloc");
 		exit(1);
 	}
 
+	spm_mt->nr_threads = threads_nr;
+	spm_mt->spm_threads = (spm_mt_thread_t *)malloc(sizeof(spm_mt_thread_t)*threads_nr);
+	if (!spm_mt->spm_threads){
+		perror("malloc");
+		exit(1);
+	}
+
+	Spms = SPM::loadMMF_mt(mmf_fname, threads_nr);
+
 	for (unsigned int i=0; i < threads_nr; i++){
-		std::cout << "************ Thread: " << i << "\n";
+		spm_mt_thread_t *spm_mt_thread;
+
+		std::cout << "==> Thread: #" << i << "\n";
 
 		Spm = Spms + i;
-		spm_mt_thread = spm_mt.spm_threads + i;
+		spm_mt_thread = spm_mt->spm_threads + i;
 
 		DrleMg = new DRLE_Manager(Spm, 4, 255-1);
 		DrleMg->EncodeAll();
-		CsxMg = new CsxManager(Spm);
-		Jit = new CsxJit(CsxMg, i);
 
-		spm_mt_thread->spm = csx = CsxMg->mkCsx();
+		CsxMg = new CsxManager(Spm);
+		spm_mt_thread->spm = CsxMg->mkCsx();
+
+		Jit = new CsxJit(CsxMg, i);
 		Jit->doHooks();
 		spm_mt_thread->spmv_fn = Jit->doJit();
 		spm_mt_thread->cpu = threads_cpus[i];
@@ -65,18 +68,59 @@ int main(int argc, char **argv)
 		delete DrleMg;
 	}
 
+	free(threads_cpus);
+	delete[] Spms;
+
+	return spm_mt;
+}
+
+static void putSpmMt(spm_mt_t *spm_mt)
+{
+	free(spm_mt->spm_threads);
+	free(spm_mt);
+}
+
+static void CheckLoop(spm_mt_t *spm_mt, char *mmf_name)
+{
+	void *crs;
 	uint64_t nrows, ncols, nnz;
-	void *crs = spm_crs32_double_init_mmf(argv[1], &nrows, &ncols, &nnz);
-	spmv_double_check_mt_loop(crs, &spm_mt,
+
+	crs = spm_crs32_double_init_mmf(mmf_name, &nrows, &ncols, &nnz);
+	std::cout << "Checking\n";
+	spmv_double_check_mt_loop(crs, spm_mt,
 	                          spm_crs32_double_multiply, 1,
 	                          nrows, ncols,
 	                          NULL);
 	spm_crs32_double_destroy(crs);
 	std::cout << "Check Passed\n";
+}
 
-	free(threads_cpus);
-	free(spm_mt.spm_threads);
-	delete[] Spms;
+static void BenchLoop(spm_mt_t *spm_mt, char *mmf_name)
+{
+	uint64_t nrows, ncols, nnz;
+	double secs, flops;
+	long loops_nr = 128;
+
+	getMmfHeader(mmf_name, nrows, ncols, nnz);
+	secs = spmv_double_bench_mt_loop(spm_mt, loops_nr, nrows, ncols, NULL);
+	flops = (double)(loops_nr*nnz*2)/((double)1000*1000*secs);
+	printf("m:%s f:%s s:%lu t:%lf r:%lf\n",
+	        "csx", basename(mmf_name), 0UL, secs, flops);
+}
+
+int main(int argc, char **argv)
+{
+	spm_mt_t *spm_mt;
+
+	if (argc < 2){
+		std::cerr << "Usage: " << argv[0] << " <mmf_file>\n";
+		exit(1);
+	}
+
+	spm_mt = getSpmMt(argv[1]);
+	CheckLoop(spm_mt, argv[1]);
+	BenchLoop(spm_mt, argv[1]);
+	putSpmMt(spm_mt);
 
 	return 0;
 }
