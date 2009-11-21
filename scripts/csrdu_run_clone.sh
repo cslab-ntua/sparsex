edb1dadcf1e92a38e982c8961f2cccfe694fa226
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2009-11-21 11:55:25 +0200

    csrdu script

--- scripts/csrdu_run_clone.sh
+++ scripts/csrdu_run_clone.sh
@@ -15,11 +15,12 @@ for mtx in $(echo $mtxfiles)
 do
 	echo "*********************** $mtx **********************"
 
-	for mt_conf in "0,2" "0,4" "0,2,4,6" "0,1,2,3,4,5,6,7"; do
-		for seq_opts in "-d 0" "-d 4" "-d 6" "-d 8" "-d 10" "-d 12"; do
-			for opts in "-j -a -t" "-j -t" "-a -t" "-t"; do
+	for mt_conf in "0" "0,2" "0,4" "0,2,4,6" "0,1,2,3,4,5,6,7"; do
+		for seq_opts in "-d 0" "-d 4" "-d 8"; do
+			for opts in "-j -a" "-j" "-a" ""; do
+				     opts="$opts -t -c"
 				     echo MT_CONF=$mt_conf scripts/csrdu_run.sh $opts $seq_opts $mtx
-				          MT_CONF=$mt_conf scripts/csrdu_run.sh $opts $seq_opts $mtx
+				          MT_CONF=$mt_conf scripts/csrdu_run.sh $opts $seq_opts $mtx | scripts/spmv_avg.py
 			done
 		done
 	done
