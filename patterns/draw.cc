1428ef082e489b556fb2815a6d107c4b76ead582
Kornilios Kourtis <kkourt@cslab.ece.ntua.gr>
2010-03-03 15:24:26 +0200

    patterns/draw: updates ... (it works now)

--- patterns/draw.cc
+++ patterns/draw.cc
@@ -11,12 +11,16 @@
 
 namespace bll = boost::lambda;
 
+extern "C"{
+	#include <unistd.h>
+}
+
 using namespace csx;
 
 void Draw(SPM &spm,
           const char *filename,
 		  int row_start, int row_end,
-		  const int width, const int height)
+		  const int width, const int height, bool symmetric=false)
 {
 	//Cairo::RefPtr<Cairo::PdfSurface> surface;
 	Cairo::RefPtr<Cairo::ImageSurface> surface;
@@ -48,6 +52,8 @@ void Draw(SPM &spm,
 	x_coord = bll::bind(coord_fn, bll::_1, (double)width, max_cols);
 	y_coord = bll::bind(coord_fn, bll::_1 - row_start, (double)height, max_rows);
 
+	cr->set_line_cap(Cairo::LINE_CAP_ROUND);
+	cr->set_line_width(2.0);
 	p_start = spm.points_begin(row_start);
 	p_end = spm.points_end(row_end);
 	for (p = p_start; p != p_end; ++p){
@@ -57,7 +63,13 @@ void Draw(SPM &spm,
 			y_pos = y_coord(elem.y);
 			//std::cout << elem << "=>" << x_pos << " " << y_pos << "(" << max_rows << ")\n";
 			cr->move_to(x_pos, y_pos);
-			cr->show_text("x");
+			cr->line_to(x_pos, y_pos);
+			if (symmetric){
+				x_pos = x_coord(elem.y);
+				y_pos = y_coord(elem.x);
+				cr->move_to(x_pos, y_pos);
+				cr->line_to(x_pos, y_pos);
+			}
 		} else {
 			Pattern::Generator *gen;
 			cr->save();
@@ -68,7 +80,7 @@ void Draw(SPM &spm,
 			while ( !gen->isEmpty() ){
 				CooElem e = gen->next();
 				cr->move_to(x_coord(e.x), y_coord(e.y));
-				cr->show_text("x");
+				// FIXME: add drawing
 			}
 			cr->restore();
 		}
@@ -78,19 +90,41 @@ void Draw(SPM &spm,
 	surface->write_to_png(filename);
 }
 
-int main(int argc, const char *argv[])
+extern int optind;
+extern char *optarg;
+
+int main(int argc, char *argv[])
 {
-	if (argc < 2){
-		std::cout << "Usage: " << argv[0] << " <mmf_file> [<out_file>]\n";
+	char *out_file = "out.png";
+	bool symmetric = false;
+	char c;
+
+	while ((c = getopt(argc, argv, "so:")) != -1){
+		switch (c) {
+			case 's':
+			symmetric = true;
+			break;
+
+			case 'o':
+			out_file = optarg;
+			break;
+
+			default:
+			fprintf(stderr, "Error parsing arguments: -%c-\n", c);
+			exit(1);
+
+		}
+	}
+
+	if (argc - optind < 1){
+		std::cout << "Usage: " << argv[0] << " [-s] [-o <out_file>] <mmf_file>\n";
 		exit(1);
 	}
 
-	const char *out_file = argc > 2 ? argv[2] : "test.png";
-
 	SPM *Spm;
 
-	Spm = SPM::loadMMF(argv[1]);
-	Draw(*Spm, out_file, 0, 0, 600, 800);
+	Spm = SPM::loadMMF(argv[optind]);
+	Draw(*Spm, out_file, 0, 0, 800, 800, symmetric);
 
 	return 0;
 }
