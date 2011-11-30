23ad76353df8536a7c6c7b0ff6070f8f427c6461
Vasileios Karakasis <bkk@cslab.ece.ntua.gr>
2011-11-30 19:20:23 +0200

    Added missing template file.

---
+++ csx/mult_templates/block_col_tmpl.c
@@ -0,0 +1,19 @@
+static inline double block_col_${r}x${c}_case(uint8_t **ctl, uint8_t size,
+                                              double **values,
+                                              double **x_curr, double **y_curr)
+{
+    register double *y_curr_ = *y_curr;
+    register double *x_curr_ = *x_curr;
+
+    for (uint8_t i = 0; i < ${r}; i++) {
+        register double yr = 0;
+        for (uint8_t j = 0; j < ${c}; j++) {
+            yr += x_curr_[j] * (**values);
+            (*values)++;
+        }
+
+        y_curr_[i] += yr;
+    }
+    
+    return 0;
+}
