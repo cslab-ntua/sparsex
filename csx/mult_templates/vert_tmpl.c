50cd6a79ab711935aa47af5411a1013ff9d03793
Vasileios Karakasis <bkk@cslab.ece.ntua.gr>
2011-11-30 16:38:01 +0200

    Changed vertical pattern multiplication pattern.

--- csx/mult_templates/vert_tmpl.c
+++ csx/mult_templates/vert_tmpl.c
@@ -3,11 +3,13 @@ static inline double vert${delta}_case(uint8_t **ctl, uint8_t size,
                                         double **x_curr, double **y_curr)
 {
     register double yr;
+    register double xr = **x_curr;
+    register uint64_t yi_end = ${delta}*size;
 
-    yr = (**x_curr) * (**values);
+    yr = xr * (**values);
     (*values)++;
-    for (uint64_t yi = ${delta}; yi < ${delta}*size; yi += ${delta}) {
-        (*y_curr)[yi] += (**x_curr) * (**values);
+    for (uint64_t yi = ${delta}; yi < yi_end; yi += ${delta}) {
+        (*y_curr)[yi] += xr * (**values);
         (*values)++;
     }
 
