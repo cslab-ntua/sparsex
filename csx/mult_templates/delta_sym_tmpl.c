c9f3f5d9baa5fd4f157dfb51866249418e010f4c
tedgoud <thgoud@webmail.cslab.ece.ntua.gr/src/>
2012-01-18 23:31:53 -0500

    Imported (after optimizing them) the multiplication functions for CSX-sym format.

---
+++ csx/mult_templates/delta_sym_tmpl.c
@@ -0,0 +1,29 @@
+static inline double delta${bits}_case(uint8_t **ctl, uint8_t size,
+                                       double **values,
+                                       double *x, double *y, double *cur,
+                                       uint64_t *x_indx, uint64_t *y_indx)
+{
+	register double yr = 0;
+	register double values_ = **values;
+	register double *x_ = x + *x_indx;
+	register double *rx_ = x + *y_indx;
+	register double *ry_ = cur + *x_indx;
+    register uint64_t jmp;
+
+	${align_ctl}
+	yr += (*x_) * values_;
+	(*ry_) += (*rx_) * values_;
+	(*values)++;
+	for (uint8_t i = 1; i < size; i++) {
+		jmp = u${bits}_get(ctl);
+		x_ += jmp;
+		ry_ += jmp;
+		(*x_indx) += jmp;
+		values_ = **values;
+		yr += (*x_) * values_;
+		(*ry_) += (*rx_) * values_;
+		(*values)++;
+	}
+
+	return yr;
+}
